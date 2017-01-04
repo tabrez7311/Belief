13c377b073ec77d787f078337984c90a60b9b118
Tabrez Chowkar <tabrezshaikh7311@gmail.com>
2017-01-04 11:42:26 +0530

    Siren Implemented

--- app/src/main/java/com/example/tabish/belief/MainActivity.java
+++ app/src/main/java/com/example/tabish/belief/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.tabish.belief;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +19,26 @@ public class MainActivity extends AppCompatActivity {
 Intent intent = new Intent(MainActivity.this,FakeCall.class);
         startActivity(intent);
     }
+
+    public void siren(View view){
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.sirensound);
+        mp.start();
+        findViewById(R.id.SirenButton).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                if(!mp.isPlaying())
+                {
+                    mp.start();
+                }
+                else{
+                    mp.seekTo(0);
+                    mp.pause();
+
+                }
+            }
+        });
+    }
+
+
 }
