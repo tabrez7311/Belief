e872bdaf969e47882137bc21998142de6000217b
Tabrez Chowkar <tabrezshaikh7311@gmail.com>
2017-01-04 20:07:31 +0530

    welcome screen button implemented

--- app/src/main/java/com/example/tabish/belief/MainActivity.java
+++ app/src/main/java/com/example/tabish/belief/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tabish.belief;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,9 +16,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean("firstTime", false)) {
+            // <---- run your one time code here
+            setContentView(R.layout.welcome);
+
+            // mark first time has runned.
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            editor.apply();
+        }
+        else {
+            setContentView(R.layout.activity_main);
+        }
+
     }
 
+    public void getStarted(View view) {
+                Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(myIntent);
+
+    }
     public void fakeCall(View view){
 Intent intent = new Intent(MainActivity.this,FakeCall.class);
         startActivity(intent);
